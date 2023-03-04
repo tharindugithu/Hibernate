f8f988326899df5e471727ea5612b4866ea3cea6
tharindugithu <atharindu325@gmail.com>
2023-03-04 19:08:52 +0530

    2nd_levelcache for HQL

--- DemoHib/src/main/java/com/tharindu/DemoHib/App.java
+++ DemoHib/src/main/java/com/tharindu/DemoHib/App.java
@@ -3,6 +3,7 @@ package com.tharindu.DemoHib;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -27,32 +28,17 @@ public class App
         SessionFactory sf = con.buildSessionFactory(reg);
         
 
-        /////////////firing query only one time because hibernate default store cache inside first level caching /////////
-        //Laptop laptop = new Laptop();
-        
-        //Transaction tx;
-        //Session session = sf.openSession();
-        //session.beginTransaction();
-        
-        //laptop = (Laptop) session.get(Laptop.class, 103);
-        //System.out.println(laptop.getLname());
-       
-        
-        //laptop = (Laptop) session.get(Laptop.class, 103);
-        //System.out.println(laptop.getLname());
-        //session.getTransaction().commit();
-        
-        
-        
-        /////////firing query two time because we need second level caching////////////////////
+        /////////by default second level cache only support get method in session not HQL . /////////////////////////
+        ///////// <property name="hibernate.cache.use_query_cache">true</property> for second level query caching////////////////////
         Laptop laptop = new Laptop();
         Transaction tx;
         
         Session session = sf.openSession();/////////create session 1////////////////////
-
         tx = session.beginTransaction();
-     
-        laptop = (Laptop) session.get(Laptop.class, 103);
+        
+        Query q1 =  session.createQuery("from Laptop where lid=101");
+        q1.setCacheable(true);
+        laptop = (Laptop) q1.uniqueResult();
         System.out.println(laptop.getLname());
         tx.commit();
         //or
@@ -63,7 +49,9 @@ public class App
         Session session2 = sf.openSession(); /////////create session 2////////////////////
         tx = session2.beginTransaction();        
         
-        laptop = (Laptop) session2.get(Laptop.class, 103);
+        Query q2 = session2.createQuery("from Laptop where lid=101");
+        q2.setCacheable(true);
+        laptop = (Laptop) q2.uniqueResult();
         System.out.println(laptop.getLname());
         tx.commit();
         //or
@@ -71,38 +59,7 @@ public class App
         session2.close();
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        /////////////get all the laps names belong to rollno = 1 student////////////////      
-        //Student s = new Student();
-        //s = (Student)session.get(Student.class, 1);
-        //List<Laptop> laptops = new ArrayList<Laptop>();
-        
-        
-        ////////////fetch laptop details///////////////////////
-        // laptops = s.getLaptop();
-        //for(Laptop l : laptops) {
-        // 	System.out.println(l.getLname());
-        //}
-        
-        
-        /////////////create a new laptop obj and assing to that lap exisiting student in db//////////////
-        //Laptop laptop = new Laptop();
-      	//Laptop laptop2 = new Laptop();
-    	//laptop.setLid(104);//create new laptop obj
-    	//laptop.setLname("HP");
-        //laptop2 =  (Laptop)session.get(Laptop.class, 103);//get student object id = 103
-        //laptop.setStudent((Student)laptop2.getStudent());//assign that student object to laptop obj
-        //session.save(laptop);//save the laptop obj db
-        
-       
-       
+
         
         
     }
